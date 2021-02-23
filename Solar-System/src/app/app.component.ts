660cd1c4fe5d83ffac9b8bd8a5fb115f861cbd3a
Ben Smith <bensmith089@gmail.com>
2021-02-22 16:03:19 -0800

    working with canvas objects

--- Solar-System/src/app/app.component.ts
+++ Solar-System/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { animate, keyframes, style } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 
 @Component({
@@ -10,11 +10,45 @@ import { Component } from '@angular/core';
     // animation triggers go here
   ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Solar-System';
   sliderValue = 50;
   rotatecss = "";
 
+  @ViewChild('canvas', { static: true })
+  canvas!: ElementRef<HTMLCanvasElement>;
+  ctx!: CanvasRenderingContext2D;
+
+  constructor(){
+
+  }
+  
+  ngOnInit(): void {
+    var canvas = document.querySelector('canvas');
+    if(canvas != undefined){
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerWidth;
+    }
+
+
+
+    this.ctx = this.canvas.nativeElement.getContext('2d') as unknown as CanvasRenderingContext2D;
+    var c = this.ctx;
+    
+    c.beginPath();
+    c.moveTo(0,0);
+    c.lineTo(200,100);
+    c.lineTo(400,400);
+    c.stroke();
+
+
+  }
+
+  animate(): void{
+    this.ctx.fillStyle = 'red';
+    const square = new Square(this.ctx);
+    square.draw(5, 1, 20);
+  }
   getSliderValue(){
     console.log(this.sliderValue);
     this.rotatecss = this.sliderValue.toString() + "deg";
@@ -22,5 +56,13 @@ export class AppComponent {
     }
   onChange(){
     
+  }
+}
+
+export class Square {
+  constructor(private ctx: CanvasRenderingContext2D) {}
+
+  draw(x: number, y: number, z: number) {
+    this.ctx.fillRect(z * x, z * y, z, z);
   }
 }
